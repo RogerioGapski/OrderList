b8caca86b053faa81bab34e5f6bdcb4251dc10fe
Rogério <rogerio.gapski.silva@gmail.com>
2026-05-04 18:24:17 -0300

    entities: order_item created

---
+++ src/main/java/com/orderList/orderList/entities/OrderItem.java
@@ -0,0 +1,36 @@
+package com.orderList.orderList.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "items_table")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer quantity;
+    private Double unitary_price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
