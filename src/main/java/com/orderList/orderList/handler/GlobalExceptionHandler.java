9f78db232e9f6e54e578e528b681d47ee795eb19
Rogério <rogerio.gapski.silva@gmail.com>
2026-05-10 02:19:29 -0300

    exceptions and handler created

--- src/main/java/com/orderList/orderList/handler/GlobalExceptionHandler.java
+++ src/main/java/com/orderList/orderList/handler/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.orderList.orderList.handler;
 
 import com.orderList.orderList.exceptions.AlreadyExistsException;
+import com.orderList.orderList.exceptions.BadRequestException;
+import com.orderList.orderList.exceptions.NotFoundException;
+import com.orderList.orderList.exceptions.UnauthorizedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +16,18 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(409).body(new ErrorResponse(e.getMessage()));
     }
 
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(BadRequestException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse(e.getMessage()));
+    }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e) {
+        return ResponseEntity.status(404).body(new ErrorResponse(e.getMessage()));
+    }
 
-
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException e) {
+        return ResponseEntity.status(403).body(new ErrorResponse(e.getMessage()));
+    }
 }
