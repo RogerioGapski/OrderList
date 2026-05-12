125fc602123cab427d0b4843d1320578ebcba26c
Rogério <rogerio.gapski.silva@gmail.com>
2026-05-11 22:44:44 -0300

    handler sintaxe fixed

--- src/main/java/com/orderList/orderList/handler/GlobalExceptionHandler.java
+++ src/main/java/com/orderList/orderList/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.orderList.orderList.exceptions.AlreadyExistsException;
 import com.orderList.orderList.exceptions.BadRequestException;
 import com.orderList.orderList.exceptions.NotFoundException;
 import com.orderList.orderList.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,21 +14,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleAlreadyExists(AlreadyExistsException e) {
-        return ResponseEntity.status(409).body(new ErrorResponse(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequest(BadRequestException e) {
-        return ResponseEntity.status(400).body(new ErrorResponse(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e) {
-        return ResponseEntity.status(404).body(new ErrorResponse(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException e) {
-        return ResponseEntity.status(403).body(new ErrorResponse(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
     }
 }
