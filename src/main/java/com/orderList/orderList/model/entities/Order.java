330916911d0b8806dc6511deed2fc3e95089e44d
Rogério <rogerio.gapski.silva@gmail.com>
2026-05-26 19:16:51 -0300

    order entity fix's

--- src/main/java/com/orderList/orderList/model/entities/Order.java
+++ src/main/java/com/orderList/orderList/model/entities/Order.java
@@ -42,10 +42,10 @@ public class Order implements Serializable {
     private Payments paymentType;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Item> items;
 
     @Override
