079c6c70b428099336c3ec22a153d15d15667847
Rogério <rogerio.gapski.silva@gmail.com>
2026-06-26 23:20:43 -0300

    token service created

--- src/main/java/com/orderlist/api/services/TokenService.java
+++ src/main/java/com/orderlist/api/services/TokenService.java
@@ -1,4 +1,56 @@
 package com.orderlist.api.services;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.orderlist.api.model.entities.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+            String token = JWT.create()
+                    .withIssuer("order-list")
+                    .withSubject(user.getId().toString())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch(JWTCreationException e){
+            throw new RuntimeException("Error creating JWT", e);
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+            return JWT.require(algorithm)
+                    .withIssuer("order-list")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch(JWTVerificationException e){
+            return "";
+        }
+    }
+
+    private Instant genExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
