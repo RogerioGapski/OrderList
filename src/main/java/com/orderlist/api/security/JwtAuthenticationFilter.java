8fdca77e76e726cd0ebacee31ac801f3a7ed9355
Rogério <rogerio.gapski.silva@gmail.com>
2026-07-06 22:38:44 -0300

    authentication filter implemented

--- src/main/java/com/orderlist/api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/orderlist/api/security/JwtAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.orderlist.api.security;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.orderlist.api.exceptions.customs.CustomAuthenticationException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import com.orderlist.api.exceptions.customs.CustomAuthenticationException.AuthErrorCode;
 
 import java.io.IOException;
 
@@ -14,6 +18,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final AuthenticationEntry entryPoint;
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
@@ -22,11 +28,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
 
+    String token = extractToken(request);
 
+    try {
+        if(token == null)
+            throw new CustomAuthenticationException(AuthErrorCode.TOKEN_MISSING);
 
+        String userId = jwtService.validateToken(token);
+        filterChain.doFilter(request, response);
 
+    } catch (TokenExpiredException e) {
+        entryPoint.commence(
+                request, response, new  CustomAuthenticationException(AuthErrorCode.TOKEN_EXPIRED));
+    } catch (JWTVerificationException e) {
+        entryPoint.commence(
+                request, response, new CustomAuthenticationException(AuthErrorCode.TOKEN_INVALID));
+    } catch (CustomAuthenticationException e) {
+        entryPoint.commence(
+                request, response, e);
+    }
+}
 
-
-
+    private String extractToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
     }
 }
